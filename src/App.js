ad84e6f8dd65e0b68b0c41cc20fd1f30f4e39192
sunny-im <hellossun.dahae@gmail.com>
2022-06-19 23:27:35 +0900

    props

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, {Component} from 'react';
 
 class App extends Component {
   state = {
-    // hello: 'hello App.js !!'
     count : 0
   };
 
-  // handleChange = () => {
   countUp = () => {
     this.setState({
-      // hello: 'bye App.js!'
       count: this.state.count + 1
     });
   };
@@ -18,12 +15,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* JSX에 변수를 넣을 때에는 중괄호{}를 사용! */}
-        {/* <div className="App">{this.state.hello}</div> */}
-        <div>{this.state.count}</div>
-        {/* <button onClick={this.handleChange}>click Me!</button> */}
-        <button onClick={this.countUp}>count Up!</button>
-        {/* 여긴 HTML 파일이 아닌 JSX 파일이므로 JSX가 HTML로 바뀌는 과정에서 함수가 실행되기 때문에 this.handleChange()가 아닌 this.handleChane 로 작성해야 한다. */}
+        <div className="props">
+          {/*Props가 들어가는 부분*/}
+          <span>{this.props.message}</span>
+        </div>
+
+        <div className="state">
+          {/*State가 들어가는 부분*/}
+          {this.state.count}
+          <button onClick={this.handleChange}>Click Me !</button>
+        </div>
       </div>
     );
   }
