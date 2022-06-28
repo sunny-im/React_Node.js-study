54336c003f8e9a6b0d48cc9e5610ea5578817518
sunny-im <hellossun.dahae@gmail.com>
2022-06-28 21:19:05 +0900

    todo_list_update

--- todo_list/src/component/TodoList.js
+++ todo_list/src/component/TodoList.js
@@ -1,32 +1,37 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState } from 'react';
 import axios from 'axios';
-import {Table} from '@material-ui/core';
+import {Table} from '@material-ui/core/';
 import {TableBody} from '@material-ui/core';
-import {TableCell, tableCellClasses} from '@material-ui/core';
+import {TableCell} from '@material-ui/core';
 import {TableContainer} from '@material-ui/core';
 import {TableHead} from '@material-ui/core';
 import {TableRow} from '@material-ui/core';
 import {Paper} from '@material-ui/core';
 import {Button} from '@material-ui/core';
-import {Checkbox} from '@material-ui/core';
+import {TextField} from '@material-ui/core/';
 import './TodoList.css';
 
-function TodoList() {
+const TodoList = React.memo(() => {
     const [datas, setDatas] = useState([]);
     const [checked, setChecked] = useState("true");
     const [addBtn, setAddBtn] = useState(true);
-    const [input, setInput] = useState({
-        id : "",
-        title : "",
-    })
+    const [newTitle, setNewTitle] = useState("");
+    const [isUpdate, setIsUpdate] = useState(false);
     
+
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/todos")
             .then(response => setDatas(response.data))
             .catch(err => console.log(err));
     }, []);
     
+    // useEffect(() => {
+    //     const list = [...datas];
+    //     const re_list = list.reverse();
+    //     console.log('솔팅팅', re_list);
+    // })
 
+    /*
     const onChange = e => {
         const {value, name} = e.target;
         setInput({
@@ -34,20 +39,30 @@ function TodoList() {
             [name] : value
         });
     };
+    */
 
     const onSave = () => {
-        setDatas([input, ...datas]);
-        setInput({
+        const newTodo = {
             userId : "1",
-            id : "",
-            title : "",
-            completed : "",
-        });
+            id: datas.length + 1,
+            title: newTitle,
+            completed: false
+        }
+        setDatas([...datas, newTodo]);
+        setNewTitle("");
     }
 
     const onDelete = id => {
         setDatas(datas.filter(item => item.id !== id));
     };
+
+    const onUpdate = () => {
+        const changeTitle = datas.map((item) => ({
+            ...item,
+            title : item.title === changeTitle.title ? isUpdate : item.title,
+        }));
+        setIsUpdate(changeTitle);
+    }
     return (
         <div className="container">
             <h2>Todo List</h2>
@@ -55,15 +70,14 @@ function TodoList() {
                 <Table sx={{ minWidth: 650 }} aria-label="customized table">
                     <TableHead>
                         <TableRow>
-                            <TableCell colSpan={5}>
+                            <TableCell colSpan={6} align="right">
                                 <Button variant="outlined" color="primary" size="small" onClick={() => setAddBtn(!addBtn)}>Add</Button>
                             </TableCell>
                         </TableRow>
                         {!addBtn&&(
                         <TableRow>
-                            <TableCell colSpan={1}></TableCell>
-                            <TableCell colSpan={3}>
-                                <input type="text" name="title" onChange={onChange} value={input.title}/>
+                            <TableCell colSpan={4}>
+                                <input type="text" name="title" onChange={(e) => setNewTitle(e.target.value)} value={newTitle} placeholder="Title"/>
                             </TableCell>
                             <TableCell colSpan={1}><Button variant="outlined" color="primary" size="small" onClick={() => {if(window.confirm(`등록할까요?`)){onSave()}}}>Submit</Button></TableCell>
                         </TableRow>
@@ -74,23 +88,34 @@ function TodoList() {
                             <TableCell align="center">Completed</TableCell>
                             <TableCell align="center">Update</TableCell>
                             <TableCell align="center">Delete</TableCell>
+                            <TableCell align="center"></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {datas.map((item) => (
                             <TableRow key={item.id} sx={{ '&:last-child td, &:last-child th': { border: 0 }}}>
                                 <TableCell align="center">{item.id}</TableCell>
-                                <TableCell align="center">{item.title}</TableCell>
-                                {/* <Tall><input type="checkbox" className={checked === {item.completed} ? "checked" : "unChecked"}/></Tall> */}                        
-                                <TableCell align="center"><input type="checkbox"/></TableCell>
-                                <TableCell align="center"><Button variant="outlined" color="primary" size="small">Update</Button></TableCell>
+                                {isUpdate===item.id?(
+                                    // <TableCell align="center"><input type="text" value={item.title} /></TableCell>
+                                    <TableCell align="center"><TextField id="filled-basic" label={item.title} variant="filled" maxRows={2}/></TableCell>
+                                ):(
+                                    <TableCell align="center">{item.title}</TableCell>
+                                )}
+                                <TableCell align="center"><input type="checkbox" checked={item.completed}/></TableCell>
+
+
+                                {/* <TableCell align="center"><Button variant="outlined" color="primary" size="small" onClick={()=>onUpdate()}>Submit</Button></TableCell> */}
+                                <TableCell align="center"><Button variant="outlined" color="primary" size="small" onClick={()=>setIsUpdate(item.id)}>Update</Button></TableCell>
+                                
+
                                 <TableCell align="center"><Button variant="outlined" color="primary" size="small" onClick={()=>{if(window.confirm(`${item.id}번째 데이터 삭제할까염?`)){onDelete(item.id)}}}>Delete</Button></TableCell>
+                                <TableCell><Button variant="outlined" color="primary" size="small">Detail</Button></TableCell> 
                             </TableRow>
-                        ))}
+                        )).reverse()}
                         </TableBody>
                 </Table>
             </TableContainer>
         </div>
     );
-}
+});
 export default TodoList;
