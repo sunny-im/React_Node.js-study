713e084a8631313f3405ed16258ffc1f3fa5dca1
sunny-im <hellossun.dahae@gmail.com>
2022-07-27 22:32:52 +0900

    class component lifecycle

--- rockscissorspaper/src/AppClass.js
+++ rockscissorspaper/src/AppClass.js
@@ -8,20 +8,32 @@ export default class AppClass extends Component {
       counter:0,
       num:1,
       value:0,
-    }
+    };
+    console.log("1. constructor안에서 state를 만든다")
   }
   increase=()=>{
     // react가 가지고 있는 setState 함수를 이용
     this.setState({
       counter:this.state.counter+1, 
-      value:this.state.value+1});
+      value:this.state.value+1
+    });
+    console.log("4.increase function", this.state);
+  }
+
+  componentDidMount(){
+    console.log("3.componentDidMount - API콜");
+  }
+
+  componentDidUpdate(){
+    console.log("6.componentDidUpdate-업데이트",this.state);
   }
   render() {
+    console.log("2/5.render - UI만들기")
     return (
       <div>
           <div>state:{this.state.counter}</div>
           <button onClick={this.increase}>클릭</button>
-          <BoxClass num={this.state.value}></BoxClass>
+          {this.state.counter <3 && <BoxClass num={this.state.value}></BoxClass>}
       </div>
     )
   }
--- rockscissorspaper/src/BoxClass.js
+++ rockscissorspaper/src/BoxClass.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 
 export default class BoxClass extends Component {
+  componentWillUnmount(){
+    console.log("7.componentWillUnmount - 종료 byebye")
+  }
   render() {
     return (
       <div>
