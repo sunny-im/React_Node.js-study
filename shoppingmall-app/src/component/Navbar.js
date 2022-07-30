1e9b1ebf98f995d5b66351cca65ca53763989348
sunny-im <hellossun.dahae@gmail.com>
2022-07-30 23:22:56 +0900

    shoppingmall-app

--- shoppingmall-app/src/component/Navbar.js
+++ shoppingmall-app/src/component/Navbar.js
@@ -2,27 +2,46 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({authenticate}) => {
   const menuList = ['여성', 'Divided', '남성', '신생아/유아', '아동', 'Home', 'Sale', '지속가능성']
+  // 3.로그인버튼을 누르면 로그인 페이지
+  const navigate = useNavigate();
+  const goToLogin=()=>{
+    navigate('/login');
+  }
+  const goToHompage=()=>{
+    navigate('/');
+  }
+  const search = (e) =>{
+    if (e.key === "Enter"){
+      // 입력한 검색어를 읽어와서
+      let keyword = e.target.value;
+      console.log(keyword)
+      // url을 바꿔준다
+      navigate(`/?q=${keyword}`)
+    }
+  };
   return (
     <div>
-        <div className='loginBtn'>
+        <div className='loginBtn'  onClick={goToLogin}>
           <FontAwesomeIcon icon={faUser} />
-          <div>로그인</div>
+          <div>{authenticate==true?"로그인":"로그아웃"}</div>
         </div>
         <div className='navBar'>
-          <img className="logo" src="/img/logo.png" alt="로고" />
+          <img className="logo" src="/img/logo.png" alt="로고" onClick={goToHompage}/>
         </div>
         <div className='menuArea'>
           <ul className='menuList'>
-            {menuList.map((menu)=> 
-              <li>{menu}</li>
+            {menuList.map((menu,idx)=> 
+              <li key={idx}>{menu}</li>
             )}
           </ul>
           <div className='search'>
             <FontAwesomeIcon icon={faSearch} />
-            <input type="text" placeholder='제품검색'/>
+            {/* 9. 엔터를 누르면 검색하기 */}
+            <input type="text" placeholder='제품검색' onKeyPress={(e)=>search(e)}/>
           </div>
         </div>
     </div>
--- shoppingmall-app/src/page/ProductAll.js
+++ shoppingmall-app/src/page/ProductAll.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { useState,useEffect } from 'react'
 import ProductCard from '../component/ProductCard';
 import {Container, Row, Col} from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
     const [productList, setProductList] = useState([]);
+    const [query, setQuery] = useSearchParams();
     const getProducts = async ()=>{
+        // 9-1. 검색 (json-server에서 q로 search하는게 포함되어 있음!)
+        let searchQuery = query.get('q') || "";
+        console.log("쿼리값은?", searchQuery);
         // 2.전체상품페이지 - 전체상품을 볼 수 있다. (json-server 사용)
-        let url='http://localhost:4000/products'
+        let url=`https://my-json-server.typicode.com/sunny-im/React_study
+        /products
+        ?q=${searchQuery}`;
         let response = await fetch(url);
         let data = await response.json();
         console.log(data);
@@ -15,13 +22,13 @@ const ProductAll = () => {
     }
     useEffect(()=>{
         getProducts()
-    },[]);
+    },[query]); // 쿼리값이 바뀔때마다 getProducts()를 호출해야 하므로!
   return (
     <div>
         <Container>
             <Row>
-                {productList.map((menu) => (
-                    <Col lg={3}>
+                {productList.map((menu, idx) => (
+                    <Col lg={3} key={idx}>
                         <ProductCard item={menu}/>
                     </Col>
                 ))}
