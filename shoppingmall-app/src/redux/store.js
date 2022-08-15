de74fa6843b4fb245aab3d8ac08cc72f944a216c
sunny-im <hellossun.dahae@gmail.com>
2022-08-15 23:43:19 +0900

    shoppingmall_ver.redux&middleware

--- shoppingmall-app/src/redux/store.js
+++ shoppingmall-app/src/redux/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index"  // rootReducer는 임의로 정한 이름
+// chrome redux devtool 사용 
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-let store = createStore(rootReducer, applyMiddleware(thunk));
+let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
