a88031348d1e35ea4ed4f6606a1046a4708ab69e
sunny-im <hellossun.dahae@gmail.com>
2022-08-22 23:50:02 +0900

    env_apiKEY_axios

--- ringgo-tv/src/redux/action/movieAction.js
+++ ringgo-tv/src/redux/action/movieAction.js
@@ -1,8 +1,10 @@
 import api from "../api";
 
+// .env파일에 숨겨둔 key값 가져오기
+const API_KEY=process.env.REACT_APP_API_KEY;
 function getMovies(){
     return async (dispatch)=>{
-        const popularMovieApi = await api.get(`/movie/popular?api_key=<<api_key>>&language=en-US&page=1`);
+        const popularMovieApi = await api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
 
 
         // let response = await fetch(url);
