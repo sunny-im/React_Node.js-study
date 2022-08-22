38c3bef2b9181cbc896873d7964986d887ef5d7c
sunny-im <hellossun.dahae@gmail.com>
2022-08-22 23:59:27 +0900

    promise.all()

--- ringgo-tv/src/redux/action/movieAction.js
+++ ringgo-tv/src/redux/action/movieAction.js
@@ -4,6 +4,7 @@ import api from "../api";
 const API_KEY=process.env.REACT_APP_API_KEY;
 function getMovies(){
     return async (dispatch)=>{
+        //fetch대신 axios로 !
         const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
 
         const topRatedApi = api.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
