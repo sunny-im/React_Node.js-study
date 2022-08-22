757f6496d37cb1bdc6b6d9b61b11285aa7e064b8
sunny-im <hellossun.dahae@gmail.com>
2022-08-22 23:58:56 +0900

    promise.all()

--- ringgo-tv/src/redux/action/movieAction.js
+++ ringgo-tv/src/redux/action/movieAction.js
@@ -4,15 +4,19 @@ import api from "../api";
 const API_KEY=process.env.REACT_APP_API_KEY;
 function getMovies(){
     return async (dispatch)=>{
-        const popularMovieApi = await api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+        const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
 
+        const topRatedApi = api.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
 
-        // let response = await fetch(url);
-        // let data = await response.json();
+        const upcomingApit = api.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
 
-        // let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`
+        // 연관이 없는 API 여러개를 await로 하나하나 부르면 기다리는 시간이 있기때문에 동시에 부르는 방법! Promise.all()
+        let [popularMovies, topRatedMovies, upcomingMovies] = await Promise.all([popularMovieApi,topRatedApi,upcomingApit]);
 
-        // let url3 = `https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`
+        console.log("popularMovies",popularMovies);
+        console.log("topRatedMovies",topRatedMovies);
+        console.log("upcomingMovies",upcomingMovies);
+
     };
 }
 export const movieAction = {
