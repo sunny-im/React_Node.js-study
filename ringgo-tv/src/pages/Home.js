0dd23af149969e790951306c91e06dae1dac5d7c
sunny-im <hellossun.dahae@gmail.com>
2022-08-25 00:23:28 +0900

    react_loading_spinners

--- ringgo-tv/src/pages/Home.js
+++ ringgo-tv/src/pages/Home.js
@@ -3,21 +3,27 @@ import { movieAction } from '../redux/action/movieAction';
 import { useDispatch, useSelector } from 'react-redux';
 import Banner from '../components/Banner';
 import MovieSlide from '../components/MovieSlide';
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Home = () => {
     const dispatch = useDispatch();
-    const {popularMovies,topRatedMovies,upcomingMovies} = useSelector(state=>state.movie);
+    const {popularMovies,topRatedMovies,upcomingMovies, loading} = useSelector(state=>state.movie);
     //console.log("home",popularMovies );
 
     useEffect(()=>{
         dispatch(movieAction.getMovies());
     },[]);
 
+    // loading이 true면 로딩스피너를 보여주고, false면 데이터를 보여준다.
+    // true : 데이터 도착 전 !!  false : 데이터 도착 후 또는 에러발생 시!!
+    if(loading){
+        return <ClipLoader color="gold" loading={loading} size={150} />;
+    }
     return (
         <div>
-            {popularMovies.results &&
+            {/* {popularMovies.results && */}
             <Banner movie={popularMovies.results[0]}/>
-            }
+            {/* } */}
             <h1>인기영화</h1>
             <MovieSlide movies={popularMovies}/>
             <h1>평점 높은 영화</h1>
--- ringgo-tv/src/redux/action/movieAction.js
+++ ringgo-tv/src/redux/action/movieAction.js
@@ -4,11 +4,13 @@ import api from "../api";
 const API_KEY=process.env.REACT_APP_API_KEY;
 function getMovies(){
     return async (dispatch)=>{
+        try {
+        // 데이터 도착 전 로딩스피너 true
+        dispatch({type:"GET_MOVIES_REQUEST"})
+
         //fetch대신 axios로 !
         const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-
         const topRatedApi = api.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
-
         const upcomingApit = api.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
 
         // 연관이 없는 API 여러개를 await로 하나하나 부르면 기다리는 시간이 있기때문에 동시에 부르는 방법! Promise.all()
@@ -18,6 +20,7 @@ function getMovies(){
             upcomingApit
         ]);
 
+        // 데이터 도착 후 로딩스피너 false
         dispatch({
             type : "GET_MOVIES_SUCCESS",
             payload : {
@@ -26,11 +29,13 @@ function getMovies(){
                 upcomingMovies:upcomingMovies.data
             }
         });
-
-        console.log("popularMovies",popularMovies);
-        console.log("topRatedMovies",topRatedMovies);
-        console.log("upcomingMovies",upcomingMovies);
-
+        // console.log("popularMovies",popularMovies);
+        // console.log("topRatedMovies",topRatedMovies);
+        // console.log("upcomingMovies",upcomingMovies);
+        } catch(error){
+            //에러 핸들링하기
+            dispatch({type:"GET_MOVIES_FAILURE"})
+        }
     };
 }
 export const movieAction = {
