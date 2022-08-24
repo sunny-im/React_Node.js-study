410a67139cbb6862e1c8cc368544048f951af76a
sunny-im <hellossun.dahae@gmail.com>
2022-08-25 00:08:35 +0900

    react-multi-carousel(slider)

--- ringgo-tv/src/pages/Home.js
+++ ringgo-tv/src/pages/Home.js
@@ -2,6 +2,7 @@ import React,{useEffect} from 'react'
 import { movieAction } from '../redux/action/movieAction';
 import { useDispatch, useSelector } from 'react-redux';
 import Banner from '../components/Banner';
+import MovieSlide from '../components/MovieSlide';
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -17,6 +18,12 @@ const Home = () => {
             {popularMovies.results &&
             <Banner movie={popularMovies.results[0]}/>
             }
+            <h1>인기영화</h1>
+            <MovieSlide movies={popularMovies}/>
+            <h1>평점 높은 영화</h1>
+            <MovieSlide movies={topRatedMovies}/>
+            <h1>개봉 예정 영화</h1>
+            <MovieSlide movies={upcomingMovies}/>
         </div>
     )
 };
