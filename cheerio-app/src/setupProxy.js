a83ee13eb6fb6188a64b03d0dd659f7f21b112ef
sunny-im <hellossun.dahae@gmail.com>
2022-09-06 23:52:03 +0900

    ProxyMiddleware

--- cheerio-app/src/setupProxy.js
+++ cheerio-app/src/setupProxy.js
@@ -1,21 +1,22 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
-	// auth 포함 하위 route에 대해서는 localhost:5000/v1을 domain으로 하여 proxy설정
     app.use(
         createProxyMiddleware('/news', {
-            target: 'https://search.naver.com',
+            target: 'https://search.naver.com/',
             changeOrigin: true,
             pathRewrite: {
-                '^/news': '' // URL ^/news -> 공백 변경
+                '^/news': '/' // URL ^/news -> 공백 변경
             }
         })
     )
-    // dummy 포함 하위 route에 대해서는 localhost:6000/v1을 domain으로 하여 proxy설정
     app.use(
-        '/remote',
+        '/naver',
         createProxyMiddleware({
-            target: 'http://remote.test.com',
+            target: 'https://api.naver.com',
             changeOrigin: true,
+            pathRewrite: {
+                '^/naver': '/' 
+            }
         }))
 }
