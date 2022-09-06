c704f2769bdca333c6344105554db29cb0b67486
sunny-im <hellossun.dahae@gmail.com>
2022-09-06 23:06:18 +0900

    cheerio news

--- cheerio-app/src/components/Menubar.js
+++ cheerio-app/src/components/Menubar.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import { Container, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Menubar = () => {
   return (
     <div>
-    <Navbar>
-        <Container className="header">
+        <div className="header">
             <Link to="/" className='nav_item'>Home</Link>
             <Link to="/news" className='nav_item'>News</Link>
-        </Container> 
-    </Navbar>
+        </div> 
     </div>
   )
 }
--- cheerio-app/src/components/News.js
+++ cheerio-app/src/components/News.js
@@ -5,53 +5,53 @@ import { Container, Box, Button, TextField, Table, TableHead, TableRow, TableCel
 
 const News = () => {
     const [search, setSearch] = useState('');
-    // const [title, setTitle] = useState([]);
-    // const [loading, setLoading] = useState(false);
-    // let newTitle = [];
+    const [title, setTitle] = useState([]);
+    const [loading, setLoading] = useState(false);
+    let newTitle = [];
 
     const searchNews = () => {
-    //     let searchItem = encodeURI(search);
-    //     axios.get(`https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query=${searchItem}`)
-    //     .then(res => {
-    //         const $ = cheerio.load(res.data);
-    //         $('.group_news > .list_news > .bx > div > div').each((index, item)=>{
-    //             const text = $(item).find('a.news_tit').text();
-    //             newTitle.push(text);
-    //         });
-    //         setTitle(newTitle);
-    //         setLoading(false);
-    //         })
-    //         .catch((error) => {
-    //         console.log("error", error);
-    //     });
+        let searchItem = encodeURI(search);
+        axios.get(`/search.naver?where=news&ie=utf8&sm=nws_hty&query=${searchItem}`)
+        .then(res => {
+            const $ = cheerio.load(res.data);
+            $('.group_news > .list_news > .bx > div > div').each((index, item)=>{
+                const text = $(item).find('a.news_tit').text();
+                newTitle.push(text);
+            });
+            setTitle(newTitle);
+            setLoading(false);
+            })
+            .catch((error) => {
+            console.log("error", error);
+        });
     console.log('123')
     };
-    // useEffect (()=>{
-        //searchNews();
-        // const timer = setInterval(() => {
-        //     setLoading((loading) => {
-        //     if (loading === 100) {
-        //         return 0;
-        //     }
-        //     const diff = Math.random() * 10;
-        //     return Math.min(loading + diff, 100);
-        //     });
-        // }, 500);
+    useEffect (()=>{
+        searchNews();
+        const timer = setInterval(() => {
+            setLoading((loading) => {
+            if (loading === 100) {
+                return 0;
+            }
+            const diff = Math.random() * 10;
+            return Math.min(loading + diff, 100);
+            });
+        }, 500);
 
-        // return () => {
-        //     clearInterval(timer);
-        // };
-    // },[]);
+        return () => {
+            clearInterval(timer);
+        };
+    },[]);
     return (
     <div>
-    <Container maxWidth="sm">
+    <Container>
         <h1 className='title'>News List</h1>
         <p>검색하고 싶은 뉴스는 ? </p> 
         <TextField id="outlined-basic" label="검색어를 입력하세요" variant="outlined" onChange={(e)=>setSearch(e.target.value)} />
         <Button variant="outlined" onClick={()=>searchNews()}>검색</Button>
-        {/* {!loading &&(
+        {!loading &&(
         <LinearProgress className="progress" variant="determinate" value={loading}/>
-        )} */}
+        )}
         <Box>
             <Table stickyHeader aria-label="sticky table">
                 <TableHead>
@@ -61,14 +61,14 @@ const News = () => {
                 </TableRow>
                 </TableHead>
                 <TableBody>
-                {/* {title.map((item,idx) => {
-                return (  */}
-                <TableRow>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
+                {title.map((item,idx) => {
+                return ( 
+                <TableRow key={idx}>
+                <TableCell>{idx+1}</TableCell>
+                <TableCell>{item}</TableCell>
                 </TableRow>
-                {/* );
-                })}  */}
+                );
+                })}
                 </TableBody>
             </Table>
         </Box>
