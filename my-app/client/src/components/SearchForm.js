ef46269ef4c176eec3a2351fc9a66fd13405f1e0
sunny-im <hellossun.dahae@gmail.com>
2022-09-21 23:55:02 +0900

    expess+react

--- my-app/client/src/components/SearchForm.js
+++ my-app/client/src/components/SearchForm.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 const SearchForm = () => {
+    const getData = () => {
+        fetch("api/data")
+        .then((res)=>{
+            return res.json();
+        })
+        .then((data)=>{
+            console.log(data);
+        })
+    }
     return (
     <div className="form">
         <input type="text" className="form-text" />
         <button
         type="button"
         className="form-btn"
-        onClick={() => {
-            fetch("api/data")
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                console.log(data);
-            });
-        }}
+        onClick={getData}
         >
         search
         </button>
