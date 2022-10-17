2ceff796f1eef37b5682c3508ada464a51f22a20
sunny-im <hellossun.dahae@gmail.com>
2022-10-17 23:37:15 +0900

    revenge 2nd

--- revengevillain 복사본/src/components/UserList.js
+++ revengevillain 복사본/src/components/UserList.js
@@ -37,7 +37,6 @@ const UserList = () => {
 
   //---------modal
   const handleOpen = (img) => {
-    console.log("asdfasdf",img)
     setOpen(true);
     setNewImg(img);
     //console.log("setNewImg",newImg)
@@ -90,8 +89,8 @@ const UserList = () => {
     <Grid>
       {!searchBtn &&(
       <Box className="searchFeild">
-        <TextField className="searchNickname" id="filled-basic" label="닉네임 입력하세요" variant="filled" />
-        <TextField className="searchUrl" id="filled-basic" label="steam url을 입력하세요" variant="filled" />
+        <TextField className="searchNickname" id="outlined-basic" label="닉네임 입력하세요" variant="outlined" size="small"/>
+        <TextField className="searchUrl" id="outlined-basic" label="steam url을 입력하세요" variant="outlined" size="small" />
       </Box>
       )}
     </Grid>
@@ -143,12 +142,14 @@ const UserList = () => {
       {!addBtn&&(
       <Grid item xs={2}>
         <Box className="addField">
-            <TextField id="outlined-textarea" label="닉네임" variant="outlined" multiline value={newNickName} onChange={(e) => setNewNickName(e.target.value)}/>
-            <TextField id="outlined-textarea" label="유형" variant="outlined" multiline name='type' value={newType} onChange={(e) => setNewType(e.target.value)}/>
-            <TextField id="outlined-textarea" label="발생일자" variant="outlined" multiline name='date' value={newDate} onChange={(e) => setNewDate(e.target.value)}/>
-            <TextField id="outlined-textarea" label="url 파라미터" variant="outlined" multiline name='parameter' value={newParameter} onChange={(e) => setNewParameter(e.target.value)}/>
-            <Button onClick={handleBtnClick} color="primary">이미지업로드</Button><input ref={imgInput} onChange={handleChange} type="file" id="fileUpload" style={{display:"none"}}/>
-            <Button variant="outlined" color="secondary" onClick={()=>{onSubmit()}}>Submit</Button>
+          <h4>New Steam User 등록</h4>
+          <TextField id="outlined-textarea" label="닉네임" variant="outlined" size="small" multiline value={newNickName} onChange={(e) => setNewNickName(e.target.value)}/>
+          <TextField id="outlined-textarea" label="유형" variant="outlined" size="small" multiline name='type' value={newType} onChange={(e) => setNewType(e.target.value)}/>
+          <TextField id="outlined-textarea" label="발생일자" variant="outlined" size="small" multiline name='date' value={newDate} onChange={(e) => setNewDate(e.target.value)}/>
+          <TextField id="outlined-textarea" label="url 파라미터" variant="outlined" size="small" multiline name='parameter' value={newParameter} onChange={(e) => setNewParameter(e.target.value)}/>
+          <Button onClick={handleBtnClick}>이미지업로드</Button>
+          <input ref={imgInput} onChange={handleChange} type="file" id="fileUpload" style={{display:"none"}}/>
+          <Button variant="outlined" onClick={()=>{onSubmit()}}>Submit</Button>
         </Box>
       </Grid>
       )}
