1aaa1c6cee092a5caa89495fbf4b70d7724feffb
sunny-im <hellossun.dahae@gmail.com>
2022-10-26 22:54:31 +0900

    component

--- revengevillain 복사본/src/components/InputBox.js
+++ revengevillain 복사본/src/components/InputBox.js
@@ -1,14 +1,8 @@
 import React,{useState, useRef}  from 'react'
 import {Container, Grid, TextField, Button, TableContainer, Table, TableHead, TableRow, TableBody, TableCell, Box, Modal} from '@material-ui/core';
 
-const InputBox = () => {
+const InputBox = (props) => {
   const [addBtn, setAddBtn] = useState(true);
-  const [steamContent, setSteamContent] = useState([]);
-  const [newNickName, setNewNickName] = useState("");
-  const [newType, setNewType] = useState("");
-  const [newDate, setNewDate] = useState("");
-  const [newParameter, setNewParameter] = useState("");
-  const [newImg, setNewImg] = useState('');
   const imgInput = useRef(null);
   const [keyword, setKeyword] = useState('');
   const [keywordList, setKeywordList] = useState([]);
@@ -47,10 +41,10 @@ const InputBox = () => {
 		<div>
 			<Box className="addField">
 				<h4>New Steam User 등록</h4>
-				<TextField id="outlined-textarea" label="닉네임" variant="outlined" size="small" multiline value={newNickName} onChange={(e) => setNewNickName(e.target.value)}/>
-				<TextField id="outlined-textarea" label="유형" variant="outlined" size="small" multiline name='type' value={newType} onChange={(e) => setNewType(e.target.value)}/>
-				<TextField id="outlined-textarea" label="발생일자" variant="outlined" size="small" multiline name='date' value={newDate} onChange={(e) => setNewDate(e.target.value)}/>
-				<TextField id="outlined-textarea" label="url 파라미터" variant="outlined" size="small" multiline name='parameter' value={newParameter} onChange={(e) => setNewParameter(e.target.value)}/>
+				<TextField id="outlined-textarea" label="닉네임" variant="outlined" size="small" multiline value={props.newNickName} onChange={(e) => props.setNewNickName(e.target.value)}/>
+				<TextField id="outlined-textarea" label="유형" variant="outlined" size="small" multiline name='type' value={props.newType} onChange={(e) => props.setNewType(e.target.value)}/>
+				<TextField id="outlined-textarea" label="발생일자" variant="outlined" size="small" multiline name='date' value={props.newDate} onChange={(e) => props.setNewDate(e.target.value)}/>
+				<TextField id="outlined-textarea" label="url 파라미터" variant="outlined" size="small" multiline name='parameter' value={props.newParameter} onChange={(e) => props.setNewParameter(e.target.value)}/>
 				<Button onClick={handleBtnClick}>이미지업로드</Button>
 				<input ref={imgInput} onChange={handleChange} type="file" id="fileUpload" style={{display:"none"}}/>
 				<Button variant="outlined" onClick={()=>{onSubmit()}}>Submit</Button>
