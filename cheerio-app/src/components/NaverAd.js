842e394979e1c4b6335117b6f7f2be5fd47f3c54
sunny-im <hellossun.dahae@gmail.com>
2022-10-31 00:13:00 +0900

    naverAds

--- cheerio-app/src/components/NaverAd.js
+++ cheerio-app/src/components/NaverAd.js
@@ -1,51 +1,64 @@
 import React, {useState, useEffect} from 'react'
 import CryptoJS from 'crypto-js';
 import axios from 'axios';
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, CircularProgress  } from '@material-ui/core';
 
 const NaverAPI = () => {
-    const[keyword, setKeyword] = useState([]);
-    const[loading, setLoading] = useState(true);
-    const method = "GET";
-    const api_url = "/ncc/campaigns";
-    const timestamp = Date.now() + '';
-    const NAVER_AD_ACCESS = process.env.REACT_APP_NAVER_AD_ACCESS;
-    const NAVER_AD_SECRET = process.env.REACT_APP_NAVER_AD_SECRET;
-    
-    const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, NAVER_AD_SECRET);
-    hmac.update(timestamp + '.' + method + '.' + api_url);
-    const hash = hmac.finalize();
-    hash.toString(CryptoJS.enc.Base64);
+  const [campaignName, setCampaignName] = useState([]);
+  const [campaignId, setCampaignId] = useState([]);
 
-    let apiRequest = {};
-    apiRequest.headers = {
-      'X-Timestamp':timestamp, 
-      'X-API-KEY': NAVER_AD_ACCESS, 
-      'X-CUSTOMER': 275956, 
-      'X-Signature': hash.toString(CryptoJS.enc.Base64)};
-    apiRequest.method = 'get';
-    apiRequest.url = '/naver/ncc/campaigns';
+  const method = "GET";
+  const timestamp = Date.now() + '';
+  const NAVER_AD_ACCESS = process.env.REACT_APP_NAVER_AD_ACCESS;
+  const NAVER_AD_SECRET = process.env.REACT_APP_NAVER_AD_SECRET;
+  const api_url = "/ncc/campaigns";
+  
+  // signature 키 만들기  (참고:https://ukcasso.tistory.com/99)
+  const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, NAVER_AD_SECRET);
+  hmac.update(timestamp + '.' + method + '.' + api_url);
+  const hash = hmac.finalize();
+  hash.toString(CryptoJS.enc.Base64);
 
-    const list = [];
-    const getCampaigns = () =>{
-        axios.request(apiRequest)
-        .then(res => {
-        console.log(res)
-        })
-        .catch((error) => {
-        console.log("error", error);
+  let apiRequest = {};
+  apiRequest.headers = {
+    'X-Timestamp':timestamp, 
+    'X-API-KEY': NAVER_AD_ACCESS, 
+    'X-CUSTOMER': 275956, 
+    'X-Signature': hash.toString(CryptoJS.enc.Base64)};
+  apiRequest.method = 'get';
+  apiRequest.url = '/naver/ncc/campaigns';
+
+  const nameList = [];
+  const IdList = [];
+  const getCampaigns = () =>{
+      axios.request(apiRequest)
+      .then(res => {
+        console.log(res.data)
+        const dataList = res.data;
+        dataList.map(item=>{
+          nameList.push(item.name);
+          IdList.push(item.nccCampaignId)
         });
-    }
+        setCampaignName(nameList);
+        setCampaignId(IdList);
+        console.log("campaignId",campaignId)
+        console.log("campaignName",campaignName)
+      })
+      .catch((error) => {
+      console.log("error", error);
+      });
+  }
 
-    useEffect (()=>{
-      getCampaigns();
-    },[]);
+  useEffect (()=>{
+    getCampaigns();
+  },[]);
 
-    return (
-        <div>
-
-        </div>
-    )
-    }
+  return (
+      <div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+  )
+  }
 
 export default NaverAPI;
