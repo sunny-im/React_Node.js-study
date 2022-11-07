96996dff6db79bf092269f5af0c888dd4c7cf129
sunny-im <hellossun.dahae@gmail.com>
2022-11-07 23:31:48 +0900

    sql 연결

--- revengevillain 복사본/server/index.js
+++ revengevillain 복사본/server/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const mysql = require("mysql");
 const app = express();
 const PORT = process.env.port || 8000;
 
+const db = mysql.createPool({
+    host : "localhost",
+    user : "root",
+    password : "1234",
+    database : "revengevillain",
+});
+
 app.get("/",(req,res)=>{
-    console.log("requested.");
+    const sqlQuery = "INSERT INTO comment (comment) VALUES ('hi')";
+    db.query(sqlQuery, (err, result)=>{
+        console.log("err",err)
+        res.send("success!");
+    })
 });
 app.listen(PORT, ()=>{
     console.log(`running on port ${PORT}`);
