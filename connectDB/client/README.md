6a0ddb76b8af35c214da0184644575b53d052000
sunny-im <hellossun.dahae@gmail.com>
2022-11-09 23:30:23 +0900

    connectDB

---
+++ connectDB/client/README.md
@@ -0,0 +1,33 @@
+## 라이브러리 / 모듈
+R
+- ckEditor 
+
+N
+- React Html Parser : HTML문자열을 React 구성요소로 변환
+  npm i node-html-parser
+
+- express : Node.js 웹 프레임워크
+  npm i express
+
+- body-Parser : Post 방식의 데이터 요청을 처리하는 모듈
+
+- mysql : RDBMS
+  npm i mysql
+
+- nodemon : 서버코드가 수정되면 서버를 자동으로 재시작
+  npm i nodemon
+  packajson - script에
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  추가 
+
+- Axios : GET, POST, PUT 등의 메서드로 API 요청
+  npm i axios
+
+- CORS : 도메인 또는 포드가 다른 서버의 데이터 요청
+  npm i cors
+
+  ## server 디렉토리
+  npm init -y
+
+  
---
+++ connectDB/server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const {urlencoded} = require('body-parser');
+const PORT = process.env.port || 8000;
+const cors = require('cors');
+
+// DB 연결 객체
+const db = mysql.createPool({
+  host : process.env.HOST,
+  user : process.env.USER,
+  password : process.env.PASSWORD,
+  database : process.env.DATABASE
+});
+
+// app.js에서 cors, express.json 사용 | extended: true => 옵션은 true일 경우 따로 설치가 필요한 npm qs 라이브러리를 사용(false일 경우 node.js에 기본적으로 내장되어있는 qs 라이브러리 사용)
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// api/get 경로로 들어온 쿼리 결과값 or 에러 출력
+app.get("/api/get", (req,res) => {
+  const sqlQuery = `SELECT * FROM ${process.env.DATABASE}`;
+  db.query(sqlQuery, (err, result) => {
+    res.send(result);
+  })
+})
+
+// 값 삽입
+app.post("/api/insert", (req,res)=>{
+  const title = req.body.title;
+  const content = req.body.content;
+  const sqlQuery = "INSERT INTO board (title,content) VALUES (?,?)";
+  db.query(sqlQuery, [title,content], (err, result)=>{
+    res.send('success!!!!');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
