8e7a2439a36d7d46160a9d87f09da732973f8be6
sunny-im <hellossun.dahae@gmail.com>
2022-11-20 22:20:48 +0900

    inputBox component

--- revengevillain 복사본/client/src/components/UserList.js
+++ revengevillain 복사본/client/src/components/UserList.js
@@ -165,16 +165,6 @@ const UserList = () => {
       </Grid>
       {!addBtn&&(
       <Grid item xs={2}>
-        {/* <Box className="addField">
-				<h4>New Steam User 등록</h4>
-				<TextField id="outlined-textarea" label="닉네임" variant="outlined" size="small" multiline name='nickName' onChange={getValue}/>
-				<TextField id="outlined-textarea" label="유형" variant="outlined" size="small" multiline name='type' onChange={getValue}/>
-				<TextField id="outlined-textarea" label="발생일자" variant="outlined" size="small" multiline name='date' onChange={getValue}/>
-				<TextField id="outlined-textarea" label="url 파라미터" variant="outlined" size="small" multiline name='parameter' onChange={getValue}/>
-				<Button onClick={handleBtnClick}>이미지업로드</Button>
-				<input ref={imgInput} onChange={handleChange} type="file" id="fileUpload" style={{display:"none"}}/>
-				<Button variant="outlined" onClick={submitContent}>Submit</Button>
-			</Box> */}
       <InputBox
         getValue={getValue}
         handleBtnClick={handleBtnClick}
