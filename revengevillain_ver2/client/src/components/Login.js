8a089ccba8b3b9090f99a6266eb918deb5d2d46c
sunny-im <hellossun.dahae@gmail.com>
2023-01-20 23:50:39 +0900

    login

--- revengevillain_ver2/client/src/components/Login.js
+++ revengevillain_ver2/client/src/components/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <h1>Simba's Login Page</h1>
+      <h1>Simba's Login Page</h1>  
       <input type="text" />
     </div>
   )
