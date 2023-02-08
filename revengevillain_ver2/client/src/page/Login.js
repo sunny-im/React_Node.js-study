46c42f027ddaf77699cc10b7b8e5e1ab9dbb7e4d
sunny-im <hellossun.dahae@gmail.com>
2023-02-08 21:44:20 +0900

    login

--- revengevillain_ver2/client/src/page/Login.js
+++ revengevillain_ver2/client/src/page/Login.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Button, Form, Container} from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { authenticateAction } from '../redux/action/authenciateAction';
 
-const Login = () => {
+const Login = ({setAuthenticate}) => {
+    const [id,setId] = useState('');
+    const [password,setPassword] = useState('');
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const loginUser=(e)=>{
+        e.preventDefault();
+        console.log("login user function")
+        setAuthenticate(true);
+        dispatch(authenticateAction.login(id,password));
+        navigate('/');
+    }
   return (
-    <div>Login</div>
+    <div>
+        <Container>
+            <Form onSubmit={(e)=>loginUser(e)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" onChange={(e)=>setId(e.target.value)}/>
+                    <Form.Text className="text-muted">
+                    e-mail
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="warning" type="submit">
+                    로그인
+                </Button>
+            </Form>
+        </Container>    
+    </div>
   )
 }
 
